0ffb9ab34733515b01662dc52a4886a1e33e44b4
tsumaethan <tsumaethan20@gmail.com>
2024-04-09 21:30:53 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 24;
+let num4 = 5;
+
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 100); // Generates a random integer greater than 0
+
+let mod = num3 % num4;
+
+let max = Math.max(5, 10, 15, 20); // Finds the highest number in the set
+
+console.log(multiply); // Output: 62
+console.log(random); // Output: (random integer greater than 0)
+console.log(mod); // Output: 4
+console.log(max); // Output: 20
