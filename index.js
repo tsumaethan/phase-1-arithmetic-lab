b218c8fd89262fb39842a387f8154ca387e4a47b
tsumaethan <tsumaethan20@gmail.com>
2024-04-09 23:35:21 +0300

    index.js

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 
-// Write your code here
-let num1 = 31;
-let num2 = 2;
-let num3 = 24;
-let num4 = 5;
+// Define num1 and num2
+const num1 = 31;
+const num2 = 2;
 
-let multiply = num1 * num2;
+// Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62.
+const multiply = num1 * num2;
 
-let random = Math.ceil(Math.random() * 100); // Generates a random integer greater than 0
+// Create a variable called random that will generate a random integer greater than 0.
+const random = Math.floor(Math.random() * 100) + 1;
 
-let mod = num3 % num4;
+// Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+const num3 = 14;
+const num4 = 10;
+const mod = num3 % num4;
 
-let max = Math.max(5, 10, 15, 20); // Finds the highest number in the set
+// Create a variable called max that finds the highest number in a set; the value returned should be 20.
+const max = Math.max(5, 10, 20);
 
-console.log(multiply); // Output: 62
-console.log(random); // Output: (random integer greater than 0)
-console.log(mod); // Output: 4
-console.log(max); // Output: 20
+// Exporting variables for testing
+module.exports = {
+  multiply,
+  random,
+  mod,
+  max
+};
